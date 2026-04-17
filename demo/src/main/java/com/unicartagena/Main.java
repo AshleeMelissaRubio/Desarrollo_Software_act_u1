ca8e04f58fa43a7db48c48d86bd609ef57473525
Ashlee <arubioz@unicartagena.edu.co>
2026-04-17 02:30:58 -0500

    feat(test): Testeo de Registro de Estudiantes.

--- demo/src/main/java/com/unicartagena/Main.java
+++ demo/src/main/java/com/unicartagena/Main.java
@@ -1,7 +1,9 @@
 package com.unicartagena;
+import com.unicartagena.models.Estudiante;
+import com.unicartagena.services.RegistroEstudianteService;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
---
+++ demo/src/test/java/testRegistroEstudiante.java
@@ -0,0 +1,21 @@
+import com.unicartagena.models.Estudiante;
+import com.unicartagena.services.RegistroEstudianteService;
+
+public class testRegistroEstudiante {
+    public static void main(String[] args) {
+        RegistroEstudianteService service = new RegistroEstudianteService();
+
+        System.out.println("=== SIMULACIÓN SPRINT 1: REGISTRO DE ESTUDIANTES ===");
+        Estudiante est1 = new Estudiante("7502420029", "1045236", "Ashlee Melissa Rubio Zapa");
+        System.out.println("Intento 1: " + service.registrarEstudiante(est1));
+
+        Estudiante est2 = new Estudiante("7502420029", "999888", "Emerejildo perez");
+        System.out.println("Intento 2: " + service.registrarEstudiante(est2));
+
+        System.out.println("\nAlumnos en el sistema:");
+        for (Estudiante e : service.listarEstudiantes()) {
+            System.out.println("- ID: " + e.getId() + " | Nombre: " + e.getNombreCompleto());
+        }
+    }
+    
+}
