28a54e34c6ac31c9061f22fc63df2d871a5a53e9
Ashlee <arubioz@unicartagena.edu.co>
2026-04-17 03:23:56 -0500

    agregando cambios.

--- demo/src/main/java/com/unicartagena/Main.java
+++ demo/src/main/java/com/unicartagena/Main.java
@@ -1,6 +1,4 @@
 package com.unicartagena;
-import com.unicartagena.models.Estudiante;
-import com.unicartagena.services.RegistroEstudianteService;
 
 public class Main {
     public static void main(String[] args) {
